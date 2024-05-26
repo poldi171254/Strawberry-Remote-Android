f340d38d415362794a9efaa3e9f1313da68c9cde
Poldi <43848359+poldi171254@users.noreply.github.com>
2024-05-26 15:04:42 +1000

    MainScreenV1

--- app/src/main/java/au/llist/strawberryremoteandroid/MainActivity.kt
+++ app/src/main/java/au/llist/strawberryremoteandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package au.llist.strawberryremoteandroid
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
--- app/src/main/java/au/llist/strawberryremoteandroid/Test.kt
+++ app/src/main/java/au/llist/strawberryremoteandroid/Test.kt
@@ -1,16 +1,11 @@
-package au.llist.strawberryremoteandroid.ui.screen
+package au.llist.strawberryremoteandroid
 
-
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -19,10 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,13 +23,15 @@ import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Home(onNavigateToPlayer: () -> Unit) {
+fun ScaffoldExample() {
+    var presses = 0
+
     Scaffold(
         topBar = {
             TopAppBar(
                 colors = topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
+                    titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = {
                     Text(
@@ -49,45 +43,42 @@ fun Home(onNavigateToPlayer: () -> Unit) {
         bottomBar = {
             BottomAppBar(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primary
+                contentColor = MaterialTheme.colorScheme.primary,
             ) {
                 Text(
-                    text = "This is the bottom"
+                    //modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
                 )
             }
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {onNavigateToPlayer()}) {
+            FloatingActionButton(onClick = { presses++ }) {
                 Icon(Icons.Default.Add, contentDescription = "Add")
-
             }
         }
     ) { innerPadding ->
         Column(
-            modifier = androidx.compose.ui.Modifier.padding(innerPadding).padding().background(color = Color.LightGray),
+            modifier = androidx.compose.ui.Modifier.padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = ("The Strawberry Remote provides very basic features to control the Strawberry Music Player:\n\n" +
-                        "     You can play the next/previous song\n" +
-                        "     You can Pause/Play a song\n\n" +
-                        "The Remote currently only works on local networks. \n" +
-                        "You will need to enable Remote Access in the Player and take note of the Ip Address and the Port the player will be listening on for a remote connection\n").trimIndent(),
-                androidx.compose.ui.Modifier.padding(30.dp),
+                //modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
 
+                    It also contains some basic inner content, such as this text.
+
+                    You have pressed the floating action button $presses times.
+                """.trimIndent(),
             )
-            Row (
-            ){
-                Text(text = "This is a row")
-            }
         }
     }
 }
 
-
 @Preview
 @Composable
-fun HomePreview() {
-    Home({})
+fun TestPreview() {
+    ScaffoldExample()
 }
