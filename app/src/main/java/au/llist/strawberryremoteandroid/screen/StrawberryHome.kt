24f2c28191785ca75187b89a56cd21f075d441b8
Poldi <43848359+poldi171254@users.noreply.github.com>
2024-09-05 14:38:53 +1000

    Added text resources

--- app/src/main/java/au/llist/strawberryremoteandroid/screen/StrawberryHome.kt
+++ app/src/main/java/au/llist/strawberryremoteandroid/screen/StrawberryHome.kt
@@ -1,25 +1,25 @@
 package au.llist.strawberryremoteandroid.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SegmentedButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.wear.compose.material.Button
 import au.llist.strawberryremoteandroid.R
 
 @Composable
@@ -38,7 +38,13 @@ fun StrawberryHome(modifier: Modifier = Modifier,
         Spacer(modifier = Modifier.height(20.dp))
         Text(stringResource(R.string.welcome_text4))
         Text(stringResource(R.string.welcome_text5), modifier = Modifier.padding(20.dp))
+
+        Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = { /*TODO*/ }) {
+
+            }
+            Button(onClick = { /*TODO*/ }) {
+            }
+        }
     }
-
-
 }
